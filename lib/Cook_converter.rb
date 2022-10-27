78b8f955dca75cce00bc11e4c17d5d495cc00afc
Праведник Александра <pravednik@sfedu.ru>
2022-10-27 04:14:06 +0300

    some Functions (2)

--- lib/Cook_converter.rb
+++ lib/Cook_converter.rb
@@ -5,30 +5,157 @@ require_relative "Cook_converter/version"
 module CookConverter
   class Error < StandardError; end
   def milliliter_in_glasses(a)
+    if (a < 0)
+      return 0
+      else
     return a/200
+    end
   end
   def liter_in_glasses(a)
-    return a/0.2
+    if (a < 0)
+      return 0
+    else
+      return a/0.2
+    end
   end
   def liter_in_tablespoon(a)
-    return a/0.015
+    if (a < 0)
+      return 0
+    else
+      return a/0.015
+    end
   end
   def milliliter_in_tablespoon(a)
-    return a/15
+    if (a < 0)
+      return 0
+    else
+      return a/15
+    end
+  end
+  def liter_in_teaspoon(a)
+    if (a < 0)
+      return 0
+    else
+      return a/0.005
+    end
+  end
+  def milliliter_in_teaspoon(a)
+    if (a < 0)
+      return 0
+    else
+      return a/5
+    end
   end
   def gram_in_glasses_of_water(m)
-    return m/250
+    if (m < 0)
+      return 0
+    else
+      return m/250
+    end
   end
   def gram_in_glasses_of_flour(m)
-    return m/160
+    if (m < 0)
+      return 0
+    else
+      return m/160
+    end
   end
   def gram_in_glasses_of_sugar(m)
-    return m/200
+    if (m < 0)
+      return 0
+    else
+      return m/200
+    end
   end
   def gram_in_glasses_of_oil(m)
-    return m/225
+    if (m < 0)
+      return 0
+    else
+      return m/225
+    end
   end
   def gram_in_glasses_of_milk(m)
-    return m/300
+    if (m < 0)
+      return 0
+    else
+      return m/300
+    end
+  end
+  def glasses_in_milliliter(a)
+    if (a < 0)
+      return 0
+    else
+      return a*200
+    end
+  end
+  def glasses_in_liter(a)
+    if (a < 0)
+      return 0
+    else
+      return a*0.2
+    end
+  end
+  def tablespoon_in_liter(a)
+    if (a < 0)
+      return 0
+    else
+      return a*0.015
+    end
+  end
+  def tablespoon_in_milliliter(a)
+    if (a < 0)
+      return 0
+    else
+      return a*15
+    end
+  end
+  def teaspoon_in_liter(a)
+    if (a < 0)
+      return 0
+    else
+      return a*0.005
+    end
+  end
+  def teaspoon_in_milliliter(a)
+    if (a < 0)
+      return 0
+    else
+      return a*5
+    end
+  end
+  def glasses_of_water_in_gram(m)
+    if (m < 0)
+      return 0
+    else
+      return m*250
+    end
+  end
+  def glasses_of_flour_in_gram(m)
+    if (m < 0)
+      return 0
+    else
+      return m*160
+    end
+  end
+  def glasses_of_sugar_in_gram(m)
+    if (m < 0)
+      return 0
+    else
+      return m*200
+    end
+  end
+  def glasses_of_oil_in_gram(m)
+    if (m < 0)
+      return 0
+    else
+      return m*225
+    end
+  end
+  def glasses_of_milk_in_gram(m)
+    if (m < 0)
+      return 0
+    else
+      return m*300
+    end
   end
 end
